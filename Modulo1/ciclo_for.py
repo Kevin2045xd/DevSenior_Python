0ef33821de68a34ee76c0eec7d13b726b4f27254
Kevin2045xd <kevincastellanoscastellan@gmail.com>
2026-03-11 20:38:23 -0500

    tablas de el 1 a el 10 con for

--- Modulo1/ciclo_for.py
+++ Modulo1/ciclo_for.py
@@ -12,10 +12,22 @@ texto = "python"
 for letra in texto:
     print(letra)
     """
-
+"""
 tabla = int(input("ingresa la tabla de multiplicar que quieres ver"))
 
 multiplicador = 1
 for x in range (1,11):
-    print(f"{tabla} x {multiplicador} = {tabla*multiplicador}" )
+    print(f"{tabla} x {multiplicador} = {tabla*multiplicador}")
     multiplicador += 1
+    """
+multiplicador = 1
+tabla = 1
+for a in range (1,11):
+    print(F"tabla de el {tabla}")
+
+    for x in range (1,11):
+        
+        print(f"{tabla} x {multiplicador} = {tabla*multiplicador}")
+        multiplicador += 1
+    multiplicador = 1
+    tabla += 1
